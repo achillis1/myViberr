3a92d31eb91a6664100ff5245c7f8522682cdb43
Ding Li <dingli@gmail.com>
2016-10-01 18:33:53 -0500

    add create model view

--- music/models.py
+++ music/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 
 class Album(models.Model):
     # user = models.ForeignKey(User, default=1)
@@ -8,6 +8,9 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.FileField(max_length=1000)
     # is_favorite = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.album_title + ' - ' + self.artist
--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
-from .models import Album
+from .models import Album, Song
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -12,4 +13,7 @@ class DetailView(generic.DetailView):
     model = Album
     template_name = 'music/detail.html'
 
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
 
