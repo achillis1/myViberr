a9c56c326eb5aa1bbdc09d5acbbab0d764a2c0b7
Ding Li <dingli@gmail.com>
2016-10-01 16:04:46 -0500

    add the favorite song form and view

--- music/models.py
+++ music/models.py
@@ -18,7 +18,7 @@ class Song(models.Model):
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
     # audio_file = models.FileField(default='')
-    # is_favorite = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.song_title
