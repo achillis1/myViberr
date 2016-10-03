d24c4de37139a395b70d31c5b6599f2e5c4b6be7
Ding Li <dingli@gmail.com>
2016-10-03 15:40:07 -0500

    default views for update/delete, but delete is not working

--- myViberr/urls.py
+++ myViberr/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^music/', include('music.urls', namespace="music")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('music.urls')),
 ]
